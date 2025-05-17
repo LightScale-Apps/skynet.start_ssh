5f9be40c7168ece93e3cae0fbfa1487f54baf578
Ryan Hillis <ryanbhillis@gmail.com>
2025-05-17 01:08:50 +0000

    updated

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/user/scripts/skynet.start_ssh
-echo "$(ip addr show | grep -w inet)" > output.txt
+echo "$(ip addr show)" > output.txt
 echo "$(ip route)" >> output.txt
 git add .
 git commit -m "updated"
